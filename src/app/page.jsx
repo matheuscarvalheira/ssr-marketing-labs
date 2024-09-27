34841da0c2518ab282a465b9bfd1d9b87b224e42
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-09-27 18:30:11 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Reviews from "./components/Reviews";
+import Services from "./components/Services";
+import Team from "./components/Team";
+
+export default function Home() {
+  return (
+    <>
+      <Header>
+        <Navbar />
+        <Banner />
+      </Header>
+      <Services />
+      <Team />
+      <Reviews />
+      <Footer />
+    </>
+  );
+}
